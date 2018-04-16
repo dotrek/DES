e62109cceec022e38ad5ad0edf5513a268df1ff1
Piotr Otapowicz <otapowiczp@tt.com.pl>
2018-04-16 11:14:30 +0200

    added key generator and array left shift method

--- src/algorithm/KeyGenerator.kt
+++ src/algorithm/KeyGenerator.kt
@@ -1,8 +1,53 @@
 package algorithm
 
+import java.util.*
+
 /**
  * Created by dotre on 15.04.2018.
  */
-class KeyGenerator(input: IntArray) {
+class KeyGenerator(private val input: IntArray) {
+    val PC1 = intArrayOf(
+            57, 49, 41, 33, 25, 17, 9,
+            1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27,
+            19, 11, 3, 60, 52, 44, 36,
+            63, 55, 47, 39, 31, 23, 15,
+            7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29,
+            21, 13, 5, 28, 20, 12, 4
+    )
+    private val rotations = intArrayOf(1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1)
+    private lateinit var key: IntArray
+    lateinit var c: IntArray
+    lateinit var d: IntArray
 
+    fun keyGenerate(): IntArray {
+        val newBits = IntArray(PC1.size)
+        for (i in 0 until newBits.size)
+            newBits[i] = input[PC1[i] - 1]
+        key = newBits
+        return key
+    }
+
+    fun getLeftSideOfKey(): IntArray {
+        c = key.copyOf(key.size / 2)
+        return c
+    }
+
+    fun getRightSideOfKey(): IntArray {
+        d = key.copyOfRange(key.size / 2, key.size)
+        return d
+    }
+
+    fun leftShift(bits: IntArray, n: Int): IntArray {
+        val answer = Arrays.copyOf(bits, bits.size)
+        for (i in 0 until n) {
+            val temp = answer[0]
+            for (j in 0 until answer.size - 1) {
+                answer[j] = answer[j + 1]
+            }
+            answer[bits.size - 1] = temp
+        }
+        return answer
+    }
 }
---
+++ test/algorithm/KeyGeneratorTest.kt
@@ -0,0 +1,61 @@
+package algorithm
+
+import org.testng.annotations.Test
+import org.testng.internal.junit.ArrayAsserts
+
+class KeyGeneratorTest {
+    val table = intArrayOf(
+            1, 2, 3, 4, 5, 6, 7, 8,
+            9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32,
+            33, 34, 35, 36, 37, 38, 39, 40,
+            41, 42, 43, 44, 45, 46, 47, 48,
+            49, 50, 51, 52, 53, 54, 55, 56,
+            57, 58, 59, 60, 61, 62, 63, 64
+    )
+
+
+    val keyGenerator = KeyGenerator(table)
+
+    @Test
+    fun checkIfKeyGeneratesProperly() {
+        val key = keyGenerator.keyGenerate()
+        ArrayAsserts.assertArrayEquals(keyGenerator.PC1, key)
+    }
+
+    @Test
+    fun checkIfLeftSideIsFirstHalfOfKey() {
+        val leftSide = intArrayOf(
+                57, 49, 41, 33, 25, 17, 9,
+                1, 58, 50, 42, 34, 26, 18,
+                10, 2, 59, 51, 43, 35, 27,
+                19, 11, 3, 60, 52, 44, 36
+        )
+
+        keyGenerator.keyGenerate()
+        val c = keyGenerator.getLeftSideOfKey()
+        ArrayAsserts.assertArrayEquals(leftSide, c)
+    }
+
+    @Test
+    fun checkIfRightSideIsSecondHalfOfKey() {
+        val rightSide = intArrayOf(
+                63, 55, 47, 39, 31, 23, 15,
+                7, 62, 54, 46, 38, 30, 22,
+                14, 6, 61, 53, 45, 37, 29,
+                21, 13, 5, 28, 20, 12, 4
+        )
+        val key = keyGenerator.keyGenerate()
+        val d = keyGenerator.getRightSideOfKey()
+        ArrayAsserts.assertArrayEquals(rightSide, d)
+    }
+
+    @Test
+    fun checkLeftShiftFunctionality() {
+        val caseArray = intArrayOf(1, 2, 3, 4, 5)
+        val caseArrayShifted = intArrayOf(2, 3, 4, 5, 1)
+        val leftShift = keyGenerator.leftShift(caseArray, 5)
+        ArrayAsserts.assertArrayEquals(caseArrayShifted, leftShift)
+    }
+}
